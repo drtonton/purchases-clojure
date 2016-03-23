f8cf385d6042c4259f980fff5ce905d5c57ac935
drtonton <tkfoster26@gmail.com>
2016-03-22 23:09:55 -0400

    finishing up

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,5 +1,28 @@
 (ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
 (defn -main []
-  (println "Hello, World!"))
+  (let [purchases (slurp purchases.csv) ; parses purchases.csv
+        purchases (str/split-lines purchases) 
+        purchases (map (fn [line] 
+                         (str/split line #",")) 
+                    purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (apply hash-map (interleave header line)))
+                       purchases)
+        purchases (walk/keywordize-key purchases)
+        (println "Choose item category: Alcohol, Shoes, Furniture, Toiletries, Food, Jewelry")
+        (def cats'("Alcohol" "Shoes" "Furniture" "Toiletries" "Food" "Jewelry"))
+        (let [text (read-line)
+              (if (= true [contains? cats text]))])]))
+                
+             
+                      
+                        
+                        
+                   
+  
